6fb9a193aa9946941cdc50b7bb4f90b397f1e09a
MatveenkovNikita <matveenkoff@gmail.com>
2021-04-13 22:01:55 +0300

    Initial Commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Никита  on 13.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
